e0fcbc5825a32b4a01028e8329382f739eeefa3b
Muhamad Reza  Abdul Rohim <reza.wikrama3@gmail.com>
2021-12-28 00:43:48 +0700

    update whatson and session

--- app/Http/Controllers/WhatsonController.php
+++ app/Http/Controllers/WhatsonController.php
@@ -9,102 +9,102 @@ use Illuminate\Support\Facades\Storage;
 
 class WhatsonController extends Controller
 {
-	public function __construct()
-	{
+    public function __construct()
+    { }
+    public function list(Request $request)
+    {
+        $offset = 0;
+        $page = 1;
+        $perPage = 10;
+        $sortDir = 'DESC';
+        $sortBy = 'id_whatson';
+        $search = null;
+        $total = 0;
+        $totalPage = 1;
 
-  }
-  public function list(Request $request){
-    $offset = 0;
-    $page = 1;
-    $perPage = 10;
-    $sortDir = 'ASC';
-    $sortBy = 'id_whatson';
-    $search = null;
-    $total = 0;
-    $totalPage = 1;
+        if ($request->page) {
+            $page = $request->page;
+        }
+        if ($request->perPage) {
+            $perPage = $request->perPage;
+        }
+        if ($request->sortDir) {
+            $sortDir = $request->sortDir;
+        }
+        if ($request->sortBy) {
+            $sortBy = $request->sortBy;
+        }
+        if ($request->search) {
+            $search = $request->search;
+        }
+        if ($page > 1) {
+            $offset = ($page - 1) * $perPage;
+        }
 
-    if ($request->page) {
-      $page = $request->page;
-    }
-    if ($request->perPage) {
-      $perPage = $request->perPage;
-    }
-    if ($request->sortDir) {
-      $sortDir = $request->sortDir;
-    }
-    if ($request->sortBy) {
-      $sortBy = $request->sortBy;
-    }
-    if ($request->search) {
-      $search = $request->search;
-    }
-    if ($page > 1) {
-      $offset = ($page - 1) * $perPage;
-    }
+        $listData = WhatsOn::orderBy($sortBy, $sortDir);
+        if ($perPage != '~') {
+            $listData->skip($offset)->take($perPage);
+        }
+        if ($search != null) {
+            $listData->where('title', 'LIKE', '%' . $search . '%')
+                ->orWhere('content', 'LIKE', '%' . $search . '%');
+        }
 
-    $listData = WhatsOn::orderBy($sortBy, $sortDir);
-    if ($perPage != '~') {
-      $listData->skip($offset)->take($perPage);
-    }
-    if ($search != null) {
-      $listData->where('title', 'LIKE', '%'.$search.'%')
-              ->orWhere('content', 'LIKE', '%'.$search.'%');
-    }
+        $listData = $listData->get();
+        if ($search) {
+            $total = WhatsOn::where('title', 'LIKE', '%' . $search . '%')
+                ->orWhere('content', 'LIKE', '%' . $search . '%')
+                ->count();
+        } else {
+            $total = WhatsOn::all()->count();
+        }
 
-    $listData = $listData->get();
-    if ($search) {
-      $total = WhatsOn::where('title', 'LIKE', '%'.$search.'%')
-                          ->orWhere('content', 'LIKE', '%'.$search.'%')
-                          ->count();
-    } else {
-      $total = WhatsOn::all()->count();
-    }
-
-    if ($perPage != '~') {
-      $totalPage = ceil($total / $perPage);
-    }
-    $res = array(
-      'status' => true,
-      'code' => 200,
-      'payload' => array(
-        'data' => $listData,
-        'page' => $page,
-        'perPage' => $perPage,
-        'sortDir' => $sortDir,
-        'sortBy' => $sortBy,
-        'search' => $search,
-        'total' => $total,
-        'totalPage' => $totalPage,
-      ),
-      'msg' => 'Success'
-    );
-    return response()->json($res, 200);
-  }
-	public function detail(Request $request){
-    $id = $request->id;
-    if($id) {
-      $data = Whatson::find($id);
-      if($data){
+        if ($perPage != '~') {
+            $totalPage = ceil($total / $perPage);
+        }
         $res = array(
-          'status' => true,
-          'code' => 200,
-          'payload' => $data,
-          'msg' => 'Success'
+            'status' => true,
+            'code' => 200,
+            'payload' => array(
+                'data' => $listData,
+                'page' => $page,
+                'perPage' => $perPage,
+                'sortDir' => $sortDir,
+                'sortBy' => $sortBy,
+                'search' => $search,
+                'total' => $total,
+                'totalPage' => $totalPage,
+            ),
+            'msg' => 'Success'
         );
-      } else {
-        $res = array(
-          'status' => false,
-          'code' => 404,
-          'msg' => 'Not Found'
-        );
-      }
-    } else {
-      $res = array(
-        'status' => false,
-        'code' => 401,
-        'msg' => 'Please provide required parameter'
-      );
+        return response()->json($res, 200);
     }
-    return response()->json($res, 200);
-  }
+    public function detail(Request $request)
+    {
+        $id = $request->id;
+        if ($id) {
+            $data = Whatson::find($id);
+            if ($data) {
+                $res = array(
+                    'status' => true,
+                    'code' => 200,
+                    'payload' => $data,
+                    'msg' => 'Success'
+                );
+            } else {
+                $res = array(
+                    'status' => false,
+                    'code' => 404,
+                    'msg' => 'Not Found'
+                );
+            }
+        } else {
+            $res = array(
+                'status' => false,
+                'code' => 401,
+                'msg' => 'Please provide required parameter'
+            );
+        }
+        return response()->json($res, 200);
+    }
 }
