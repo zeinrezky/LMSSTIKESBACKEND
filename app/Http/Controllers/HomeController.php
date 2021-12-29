f895123c8debf754eec86d36d257dbd029c2edb2
Muhamad Reza  Abdul Rohim <reza.wikrama3@gmail.com>
2021-12-29 22:09:19 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,46 +11,45 @@ use App\Announcement;
 
 class HomeController extends Controller
 {
-	public function __construct()
-	{
+    public function __construct()
+    { }
+    public function index(Request $request)
+    {
+        $knowledge_limit = 5;
+        $whatson_limit = 5;
+        $campus_directory_limit = 5;
+        $campus_about_limit = 5;
 
-	}
-	public function index(Request $request){
-    $knowledge_limit = 5;
-    $whatson_limit = 5;
-    $campus_directory_limit = 5;
-    $campus_about_limit = 5;
+        if ($request->knowledge)
+            $knowledge_limit = $request->knowledge;
+        if ($request->whatson)
+            $whatson_limit = $request->whatson;
+        if ($request->campus_directory)
+            $campus_directory_limit = $request->campus_directory;
+        if ($request->campus_about)
+            $campus_about_limit = $request->campus_about;
 
-    if($request->knowledge)
-      $knowledge_limit = $request->knowledge;
-    if($request->whatson)
-      $whatson_limit = $request->whatson;
-    if($request->campus_directory)
-      $campus_directory_limit = $request->campus_directory;
-    if($request->campus_about)
-      $campus_about_limit = $request->campus_about;
-    
-    $knowledge = Knowledge::limit($knowledge_limit)->get();
-    $whatson = Whatson::limit($whatson_limit)->get();
-    $campus_dir = Campus::limit($campus_directory_limit)->where('type','DIR')->get();
-    $campus_about = Campus::limit($campus_about_limit)->where('type','ABOUT')->get();
-    $announcement = Announcement::where('status','ACTIVE')->get();
-    $profile = $request->auth;
+        $knowledge = Knowledge::limit($knowledge_limit)->get();
+        $whatson = Whatson::limit($whatson_limit)->orderBy("id", "desc")->get();
+        $campus_dir = Campus::limit($campus_directory_limit)->where('type', 'DIR')->get();
+        $campus_about = Campus::limit($campus_about_limit)->where('type', 'ABOUT')->get();
+        $announcement = Announcement::where('status', 'ACTIVE')->get();
+        $profile = $request->auth;
 
-    $payload = array(
-      'announcement' => $announcement,
-      'campus_about' => $campus_about,
-      'campus_directory' => $campus_dir,
-      'knowledge' => $knowledge,
-      'profile' => $request->auth,
-      'whatson' => $whatson
-    );
-    $res = array(
-      'status' => true,
-      'code' => 200,
-      'payload' => $payload,
-      'msg' => 'Success'
-    );
-    return response()->json($res, 200);
-  }
+        $payload = array(
+            'announcement' => $announcement,
+            'campus_about' => $campus_about,
+            'campus_directory' => $campus_dir,
+            'knowledge' => $knowledge,
+            'profile' => $request->auth,
+            'whatson' => $whatson
+        );
+        $res = array(
+            'status' => true,
+            'code' => 200,
+            'payload' => $payload,
+            'msg' => 'Success'
+        );
+        return response()->json($res, 200);
+    }
 }
